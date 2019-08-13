63f9bf2de192af118187610c40789d787fb17200
glpla <1942194789@qq.com>
2019-08-13 22:22:30 +0800

    upload and history

--- miniprogram/pages/album/album.js
+++ miniprogram/pages/album/album.js
@@ -1,65 +1,86 @@
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    rating: 3
+    msg: '',
+    rating: 3,
+    imgs: []
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  uploadImg() {
+    wx.chooseImage({
+      count: 9,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success: res => {
+        // tempFilePath可以作为img标签的src属性显示图片
+        const tempFilePaths = res.tempFilePaths
+        this.setData({
+          imgs: res.tempFilePaths
+        })
+      }
+    })
+  },
+  previewImg(e) {
+    let src = e.currentTarget.dataset.src;
+    wx.previewImage({
+      current: src,
+      urls: this.data.imgs
+    })
+  },
+  history() {
+    wx.navigateTo({
+      url: '../history/history',
+    })
+  },
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
