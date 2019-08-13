ee6b7d3cf4a0a56154f8faaef67f9877438dd2c8
glpla <1942194789@qq.com>
2019-08-14 00:38:36 +0800

    page album and history: done, todo: css

--- miniprogram/pages/album/album.js
+++ miniprogram/pages/album/album.js
@@ -1,10 +1,18 @@
+const util = require('../../utils/util.js');
+const app = getApp();
+const db = wx.cloud.database();
 Page({
   data: {
     msg: '',
-    rating: 3,
-    imgs: []
+    imgs: [],
+    fileID: []
   },
-
+  change(e) {
+    console.log(e.detail)
+    this.setData({
+      msg: e.detail
+    })
+  },
   uploadImg() {
     wx.chooseImage({
       count: 9,
@@ -26,60 +34,92 @@ Page({
       urls: this.data.imgs
     })
   },
+  submit() {
+    wx.showToast({
+      title: '提交中',
+    })
+    let promissAll = []
+    this.data.imgs.forEach((item, index) => {
+      // console.log(index)
+      promissAll.push(
+        new Promise((resolve, reject) => {
+          wx.cloud.uploadFile({
+            cloudPath: index + '.jpg',
+            filePath: this.data.imgs[index],
+            success: res => {
+              // console.log(res.fileID)
+              this.setData({
+                //数组追加哦,否则后面会覆盖前面
+                fileID: this.data.fileID.concat(res.fileID)
+              })
+              //返回处理结果
+              resolve()
+            },
+            fail: console.error
+          })
+        })
+      )
+    })
+    Promise.all(promissAll).then(res => {
+      //所有的上传都结束了才会执行入库操作
+      // console.log('upload done')
+      db.collection('daily').add({
+          data: {
+            time: util.formatTime(new Date()),
+            msg: this.data.msg,
+            fileID: this.data.fileID
+          }
+        })
+        .then(res => {
+          wx.hideToast()
+          wx.showToast({
+            title: '提交成功',
+          })
+        })
+        .catch(err => {
+          console.error(err)
+        })
+    }).catch()
+  },
   history() {
     wx.navigateTo({
       url: '../history/history',
     })
   },
   onLoad: function(options) {
-
+    console.log(util.formatTime(new Date()))
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
   onReady: function() {
 
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
+
   onShow: function() {
 
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
+
   onHide: function() {
 
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
+
   onUnload: function() {
 
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
+
   onPullDownRefresh: function() {
 
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
+
   onReachBottom: function() {
 
   },
 
-  /**
-   * 用户点击右上角分享
-   */
+
   onShareAppMessage: function() {
 
   }
